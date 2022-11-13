193de7561719c6540506110ae36a6697e5d67b0b
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-11-13 17:31:14 +0100

    feat: 🎸 Add root queries

--- apps/graphql-api/src/schema/schema.ts
+++ apps/graphql-api/src/schema/schema.ts
@@ -91,3 +91,79 @@ const UserType = new GraphQLObjectType({
         phone: { type: GraphQLString },
     }),
 });
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        // PRODUCT
+        Products: {
+            type: new GraphQLList(ProductType),
+            resolve(parent, args) {
+                return Product.find();
+            },
+        },
+        Product: {
+            type: ProductType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Product.findById(args.id);
+            },
+        },
+        // INVITE
+        Invites: {
+            type: new GraphQLList(InviteType),
+            resolve(parent, args) {
+                return Invite.find();
+            },
+        },
+        Invite: {
+            type: InviteType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Invite.findById(args.id);
+            },
+        },
+        // PRODUCT ADDITIONAL INFO
+        ProductAdditionalInfos: {
+            type: new GraphQLList(ProductAdditionalInfoType),
+            resolve(parent, args) {
+                return ProductAdditionalInfo.find();
+            },
+        },
+        ProductAdditionalInfo: {
+            type: ProductAdditionalInfoType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return ProductAdditionalInfo.findById(args.id);
+            },
+        },
+        // PRODUCT IMAGES
+        ProductImages: {
+            type: new GraphQLList(ProductImageType),
+            resolve(parent, args) {
+                return ProductImage.find();
+            },
+        },
+        ProductImage: {
+            type: ProductImageType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return ProductImage.findById(args.id);
+            },
+        },
+        // USERS
+        Users: {
+            type: new GraphQLList(UserType),
+            resolve(parent, args) {
+                return User.find();
+            },
+        },
+        User: {
+            type: UserType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return User.findById(args.id);
+            },
+        },
+    },
+  });
