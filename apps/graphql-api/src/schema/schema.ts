57cb8dcf548970de6358d45a2f31c165130aa6f6
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-11-13 17:26:48 +0100

    feat: 🎸 add schema

---
+++ apps/graphql-api/src/schema/schema.ts
@@ -0,0 +1,93 @@
+import Invite from "../data/Invite";
+import Product from "../data/Product";
+import User from "../data/User";
+import ProductAdditionalInfo from "../data/ProductAdditionalInfo";
+import ProductImage from "../data/ProductImage";
+
+import {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLEnumType,
+  GraphQLInt,
+} from "graphql";
+
+// INVITE
+const InviteType = new GraphQLObjectType({
+    name: "User",
+    fields: () => ({
+        id: { type: GraphQLID },
+        invitee_id: { 
+            type: UserType,
+            resolve(parent, args) {
+                return User.findById(parent.invitee_id)
+            }
+        },
+        invited_email: { type: GraphQLString },
+        // token: { type: GraphQLString },
+        // expiration: { type: GraphQLString },
+    }),
+});
+
+// PRODUCT 
+const ProductType = new GraphQLObjectType({
+    name: "Product",
+    fields: () => ({
+        id: { type: GraphQLID },
+        product_name: { type: GraphQLString },
+        product_sub_title: { type: GraphQLString },
+        product_description: { type: GraphQLString },
+        main_category: { type: GraphQLString },
+        sub_category: { type: GraphQLString },
+        price: { type: GraphQLInt },
+        link: { type: GraphQLString },
+        overall_rating: { type: GraphQLString },
+    }),
+});
+
+// PRODUCT ADDITIONAL INFO
+const ProductAdditionalInfoType = new GraphQLObjectType({
+    name: "ProductAdditionalInfo",
+    fields: () => ({
+        id: { type: GraphQLID },
+        product_id: { 
+            type: ProductType,
+            resolve(parent, args) {
+                return Product.findById(parent.product_id)
+            }
+        },
+        choices: { type: GraphQLString },
+        additional_info: { type: GraphQLString },
+    }),
+});
+
+// PRODUCT ADDITIONAL INFO
+const ProductImageType = new GraphQLObjectType({
+    name: "ProductImage",
+    fields: () => ({
+        id: { type: GraphQLID },
+        product_id: { 
+            type: ProductType,
+            resolve(parent, args) {
+                return Product.findById(parent.product_id)
+            }
+        },
+        image_url: { type: GraphQLString },
+        alt_text: { type: GraphQLString },
+        additional_info: { type: GraphQLString },
+    }),
+});
+
+// USER 
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString },
+    }),
+});
