b938361bbc5b1a8882bdf97baa7263902b338ed9
Kwandes <bz.jasiek@gmail.com>
2022-11-13 01:54:54 +0100

    Add Sendgrid integration

--- email-integration/routes/emails.js
+++ email-integration/routes/emails.js
@@ -1,3 +1,5 @@
+import sgMail from "@sendgrid/mail";
+import "dotenv/config";
 import { Router } from "express";
 
 export const emailsRouter = Router();
@@ -22,11 +24,13 @@ emailsRouter.post("/invite/:receipientsEmail", (req, res) => {
     });
   }
   // Process the email
-  res.send({
+  res.status(202).send({
     message: "Sending an invite email based on a template",
   });
 });
 
+// TODO - add ability to pass the title and body
+// TODO - write the Readme
 emailsRouter.post("/custom/:receipientsEmail", (req, res) => {
   const receiptient = req.params.receipientsEmail;
   // Validate the email
@@ -40,8 +44,46 @@ emailsRouter.post("/custom/:receipientsEmail", (req, res) => {
     });
   }
 
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: receiptient,
+    from: process.env.SENDGRID_FROM_DOMAIN,
+    subject: "ALERT! You've been attacked",
+    text: "The blackwall netrunners are currently diving into your ICE and trying to break through!",
+    html: `<strong>!!! WARNING !!! Malware detected!</strong>
+    <br>
+    Your computer has been attacked by (3) daemons. Our system detected: (2) attempt(s) to obtain your personal data, (1) attempt(s) to corrupt your neural processor.
+    <br>
+    <strong>IMMEDIATE ACTION REQUIRED.</strong>
+    <br>
+    Our quick threat neutralization software can prevent identity theft and implant hijacking. Remember! Hostile netrunners can infiltrate your nervous system and force you to commit terrible crimes such as:
+    <br>
+    - fraudulent bank transfers
+    <br>
+    - assault
+    <br>
+    - theft and/or armed robbery
+    <br>
+    - suicide
+    <br>
+    To protect yourself, click the SCAN NOW button. Our sophisticated anti-daemon software will eliminate all threats immediately!
+    <br>
+    If you do not scan in the next (5) minute(s) (17) second(s), your operating system may suffer irreversible damage. Protect yourself now!`,
+  };
+
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode);
+    })
+    .catch((error) => {
+      console.error(error);
+
+      console.error(error.response.body);
+    });
+
   // Process the email
-  res.send({
+  res.status(202).send({
     message: "Sending an email with your custom content",
   });
 });
