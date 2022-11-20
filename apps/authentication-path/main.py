bfd84123d3b06c3c0f515ee535ee03ce9681bf3a
DryDish <57399961+DryDish@users.noreply.github.com>
2022-11-20 22:20:42 +0100

    add wip routes

--- apps/authentication-path/main.py
+++ apps/authentication-path/main.py
@@ -1,6 +1,15 @@
 #!/bin/env python
 from fastapi import FastAPI, Response, status
-from model.objects import LoginObject, SignupObject, InviteObject, ValidateObject
+from model.objects import (
+    LoginObject,
+    LoginRes,
+    SignupObject,
+    SignupRes,
+    InviteObject,
+    InviteRes,
+    ValidateObject,
+    ValidateRes,
+)
 from fastapi.middleware.cors import CORSMiddleware
 from config.secrets import get_env, validate_envs
 from utility.rabbitmq import RabbitMqConnection
@@ -8,25 +17,25 @@ from utility.logger import log
 import uvicorn
 import json
 
-from utility.result import Err
+from utility.result import Err, Ok
 
 
 server = FastAPI()
 
 
-@server.post("/auth/validate")
+@server.post("/auth/validate", response_model=ValidateRes)
 def validate_route(validate_obj: ValidateObject, response: Response):
     log.info(f"/auth/validate has been called with: '{validate_obj.__dict__}'")
     rmq = RabbitMqConnection()
 
     message = json.dumps(validate_obj.__dict__)
-    ok_reply = {'ok': 'validated'}
-    err_reply = {'error': 'someError', 'detail': 'Some detail info here'}
+    ok_reply = {"ok": "validated"}
+    err_reply = {"error": "someError", "detail": "Some detail info here"}
 
     result = rmq.publish_message_and_receive_response(
         queue="validate-token",
         message=message,
-        response=err_reply,
+        response=ok_reply,
     )
 
     if result.is_ok():
@@ -39,12 +48,10 @@ def validate_route(validate_obj: ValidateObject, response: Response):
             return reason
     else:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return result.err()
+        return result.__dict__
 
 
-
-
-@server.post("/auth/login")
+@server.post("/auth/login", response_model=LoginRes)
 def login_route(login_obj: LoginObject, response: Response):
     log.info(f"/auth/login has been called with: '{login_obj.__dict__}'")
     rmq = RabbitMqConnection()
@@ -66,26 +73,51 @@ def login_route(login_obj: LoginObject, response: Response):
             return {"error": "unauthorized"}
     else:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return result.err()
+        return result.__dict__
 
 
-@server.post("/auth/signup")
-def signup_route(signup_obj: SignupObject):
+@server.post("/auth/signup", response_model=SignupRes)
+def signup_route(signup_obj: SignupObject, response: Response):
     log.info(f"/auth/signup has been called with: '{signup_obj.__dict__}'")
+    rmq = RabbitMqConnection()
 
-    # Send signup information to user-service
+    message = json.dumps(signup_obj.__dict__)
+    reply = {}
+    if signup_obj.email == "taken@email.com":
+        reply = Err("IntegrityError", f"Email '{signup_obj.email}' is is already signed up").__dict__
+    else:
+        reply = Ok(signup_obj.__dict__).__dict__
 
-    # get response back
+    result = rmq.publish_message_and_receive_response(
+        queue="login-user",
+        message=message,
+        response=reply,
+    )
 
-    # respond with success or error - validation is likely to throw errors
-    return signup_obj
+    if result.is_ok():
+        data = result.data()
+        if data.get("ok"):
+            response.status_code = status.HTTP_201_CREATED
+            return data.get("ok")
+        else:
+            response.status_code = status.HTTP_409_CONFLICT
+            return data
+    else:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return result.__dict__
 
 
-@server.post("/auth/accept-invite")
-def accept_invite_route(invite_obj: InviteObject):
+@server.post("/auth/accept-invite", response_model=InviteRes)
+def accept_invite_route(invite_obj: InviteObject, response: Response):
     log.info(f"/auth/accept-invite has been called with: '{invite_obj.__dict__}'")
 
-    # lets get here when we get here...
+    # TODO: Figure this one out
+    # Send the request over to user-service
+    ## On the user-service:
+    #   Check if an invite of invite_obj exists
+    #   if it does - mark it as accepted - SOMEHOW
+    #   return - ????????
+
 
     return invite_obj
 
@@ -104,9 +136,7 @@ if __name__ == "__main__":
         allow_methods=["*"],
         allow_headers=["*"],
     )
-    print(
-        f"Server will run on port '{port}' on host '{host}'. Autoreload enabled?: '{will_reload}'."
-    )
+    print(f"Server will run on port '{port}' on host '{host}'. Autoreload enabled?: '{will_reload}'.")
     uvicorn.run(
         "main:server",
         port=port,
--- apps/authentication-path/model/objects.py
+++ apps/authentication-path/model/objects.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
+import uuid
 
 
 class LoginObject(BaseModel):
     email: str
     password: str
+
+
+class LoginRes(BaseModel):
+    token: str
 
 
 class SignupObject(BaseModel):
@@ -12,11 +17,25 @@ class SignupObject(BaseModel):
     password: str
 
 
+class SignupRes(BaseModel):
+    user_id: uuid.UUID
+    email: str
+    name: str
+
+
 class InviteObject(BaseModel):
     from_email: str
     to_email: str
     token: str
 
 
+class InviteRes(BaseModel):
+    status_question_mark: str
+
+
 class ValidateObject(BaseModel):
     token: str
+
+
+class ValidateRes(BaseModel):
+    status: str
