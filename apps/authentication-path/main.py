ed7df53e41fe020003c13e9cba84a4a447454069
DryDish <57399961+DryDish@users.noreply.github.com>
2022-11-20 04:16:24 +0100

    add placeholder rabbitmq functionality

--- apps/authentication-path/main.py
+++ apps/authentication-path/main.py
@@ -1,40 +1,80 @@
 #!/bin/env python
-from fastapi import FastAPI
+from typing import Any
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
 from config.secrets import get_env, validate_envs
+from model.objects import LoginObject, SignupObject, InviteObject
+from utility.rabbitmq import RabbitMqConnection
+from utility.logger import log
 import uvicorn
-import logging
 
-
-log = logging.getLogger("uvicorn.info")
 
 server = FastAPI()
 
+
 @server.post("/auth/login")
-def login_route():
-    log.info("/auth/login has been called")
-    return {"hello_from": "/auth/login"}
+def login_route(login_obj: LoginObject, response: Response):
+    log.info(f"/auth/login has been called with: '{login_obj.__dict__}'")
+    hello_connection = RabbitMqConnection("localhost")
+    result = hello_connection.publish_message("hello", login_obj)
+    
+    # send login information to user-service
+    if result.is_err():
+        response.status_code = 500
+        return {"error": result.error, "detail": result.detail}
+
+    result = hello_connection.consume_message(queue="hello")
+    # get token or error from user-service
+
+    print(result)
+    # process response
+
+    return {"token": "token"} or {"error": "error", "detail": "detail"}
+
 
 @server.post("/auth/signup")
-def signup_route():
-    log.info("/auth/login has been called")
-    return {"hello_from": "/auth/signup"}
+def signup_route(signup_obj: SignupObject):
+    log.info(f"/auth/signup has been called with: '{signup_obj.__dict__}'")
+
+    # Send signup information to user-service
+
+    # get response back
+
+    # respond with success or error - validation is likely to throw errors
+    return signup_obj
 
 
 @server.post("/auth/accept-invite")
-def accept_invite_route():
-    log.info("/auth/login has been called")
-    return {"hello_from": "/auth/accept-invite"}
+def accept_invite_route(invite_obj: InviteObject):
+    log.info(f"/auth/accept-invite has been called with: '{invite_obj.__dict__}'")
+
+    # lets get here when we get here...
+
+    return invite_obj
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     validate_envs().data()
-    port = int(get_env('AUTHENTICATION_PATH_PORT'))
-    host = get_env('AUTHENTICATION_PATH_HOST')
+    port = int(get_env("AUTHENTICATION_PATH_PORT"))
+    host = get_env("AUTHENTICATION_PATH_HOST")
     will_reload = bool(int(get_env("RELOAD_UVICORN")))
-    print(f"Server will run on port '{port}' on host '{host}'. Autoreload enabled?: '{will_reload}'.")
+    
+    
+    origins = [f"http://{host}:{port}"]
+    server.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    print(
+        f"Server will run on port '{port}' on host '{host}'. Autoreload enabled?: '{will_reload}'."
+    )
     uvicorn.run(
         "main:server",
         port=port,
         host=host,
         reload=will_reload,
+        headers=[("ContentType", "application/json")],
     )
---
+++ apps/authentication-path/utility/rabbitmq.py
@@ -0,0 +1,70 @@
+import socket
+from typing import TypeVar, Any
+from pydantic import BaseModel
+from pika.exceptions import AMQPConnectionError
+from utility.logger import log
+from utility.result import Err, Ok
+from pika.adapters.blocking_connection import BlockingChannel
+from pika import BlockingConnection
+import pika
+import json
+
+
+T = TypeVar("T", bound=type(BaseModel))
+
+
+class RabbitMqConnection:
+    host: str
+    connection: BlockingConnection
+    channel: BlockingChannel
+
+    def __init__(self, host: str) -> None:
+        self.host = host
+        try:
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+            self.channel = self.connection.channel()  # need to set
+            self.connection.close()
+        except AMQPConnectionError as error:
+            log.warn(f"Failed to connect to the RabbitMq Server.")
+
+    def publish_message(self, queue, message: T):
+        try:
+            self.connection = BlockingConnection(pika.ConnectionParameters(self.host))
+            self.channel = self.connection.channel()
+        except AMQPConnectionError as err:
+            log.error("Unable to reach RabbitMQ server!")
+            log.error(err.args[0])
+            return Err(
+                "InternalCommunicationError",
+                "Unable to communicate with RabbitMQ server.",
+            )
+
+        self.channel.queue_declare(queue=queue)
+
+        json_msg = json.dumps(message.__dict__)
+        self.channel.basic_publish(exchange="", routing_key=queue, body=json_msg)
+        return Ok("")
+
+    def consume_message(self, queue):
+        try:
+            self.connection = BlockingConnection(pika.ConnectionParameters(self.host))
+            self.channel = self.connection.channel()
+        except AMQPConnectionError as err:
+            log.error("Unable to reach RabbitMQ server!")
+            log.error(err.args[0])
+            return Err(
+                "InternalCommunicationError",
+                "Unable to communicate with RabbitMQ server.",
+            )
+        response = []
+
+        method_frame, header_frame, body = self.channel.basic_get(queue=queue)
+        print(method_frame, header_frame, body)
+        if method_frame is None:
+            self.connection.close()
+        else:
+            self.channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+            self.connection.close()
+            response.append(body)
+        
+        return response
