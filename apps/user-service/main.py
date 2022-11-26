81ecb528e80c14a6c116ae788d9aa5436e403568
DryDish <57399961+DryDish@users.noreply.github.com>
2022-11-26 13:50:04 +0100

    add missing friend_path rpc deinitions

--- apps/user-service/main.py
+++ apps/user-service/main.py
@@ -1,5 +1,5 @@
 #!/bin/env python
-from wrapper.friend_path import check_user_is_invited
+from wrapper.friend_path import check_token_is_valid, check_user_is_invited, get_user_friends
 from config.database import DB_CONNECTION
 from utility.functions import initialize_db
 from model.User import User
@@ -9,6 +9,11 @@ from utility import rabbitmq
 from sys import exit
 
 
+def subscribe_friend_path_rpc():
+    rabbitmq.subscribe("user.check.invited", check_user_is_invited)
+    rabbitmq.subscribe("token.check.valid", check_token_is_valid)
+    rabbitmq.subscribe("user.get.friends", get_user_friends)
+
 def main():
     validate_envs().data()
     result = initialize_db(DB_CONNECTION, [User, Invite])
@@ -16,7 +21,7 @@ def main():
         print(f"DB Connection Failed! Reason: {result.err()}\nExiting...")
         exit(2)
 
-    rabbitmq.subscribe("user.check.invited", check_user_is_invited)
+    subscribe_friend_path_rpc()
 
     exit(0)
 
