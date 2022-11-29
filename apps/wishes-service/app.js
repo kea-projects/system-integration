6129fce32bb95ebbcbbc35f045dcc3985be78d1d
Kwandes <bz.jasiek@gmail.com>
2022-11-29 11:58:28 +0100

    Set up database and its queries

--- apps/wishes-service/app.js
+++ apps/wishes-service/app.js
@@ -1,6 +1,7 @@
 import chalk from "chalk";
 import "dotenv/config";
 import express from "express";
+import { initDatabase } from "./database/database.service.js";
 
 const PORT = process.env.SERVER_PORT || 8080;
 const app = express();
@@ -17,6 +18,8 @@ app.listen(PORT, (error) => {
   if (error) {
     console.error(error);
   }
+
+  initDatabase();
 
   console.log(
     chalk.yellowBright("Wishes Server has started on port:"),
