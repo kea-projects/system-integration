6e0447a83c15068516b5bcd296d129cf42589782
DryDish <57399961+DryDish@users.noreply.github.com>
2022-12-06 03:00:40 +0100

    add refactored nginx config to prepare for ssl

--- configs/gateway/gateway.dockerfile
+++ configs/gateway/gateway.dockerfile
@@ -1,10 +1,19 @@
-FROM nginx:stable-alpine
+FROM nginx:stable
 
+# Install certbot requirements
+RUN apt-get update && apt-get install certbot python3-certbot-nginx cron -y
 
-COPY ./configs/gateway/root.conf                                    /etc/nginx/nginx.conf
-COPY ./configs/gateway/conf.d/api2.gifts.hotdeals.dev.conf          /etc/nginx/conf.d/api2.gifts.hotdeals.dev
-# COPY ./configs/gateway/conf.d/1_container-map.conf                  /etc/nginx/conf.d/1_container-map.conf
+RUN mkdir /scripts
+COPY ./configs/gateway/prepare-certbot.sh /scripts
+
+RUN rm /etc/nginx/conf.d/default.conf
+# Copy script for later usage
 COPY ./configs/gateway/conf.d/stream/default.conf                   /etc/nginx/conf.d/stream/
 COPY ./configs/gateway/conf.d/stream/container-map.conf             /etc/nginx/conf.d/stream/
+COPY ./configs/gateway/conf.d/1_container-map.conf                  /etc/nginx/conf.d/1_container-map.conf 
+COPY ./configs/gateway/conf.d/api2.gifts.hotdeals.dev.conf          /etc/nginx/conf.d/api2.gifts.hotdeals.dev.conf
+COPY ./configs/gateway/root.conf                                    /etc/nginx/nginx.conf
+
+
 
 CMD ["/bin/sh", "-c", "exec nginx -g 'daemon off;';"]
--- configs/gateway/prepare-certbot.sh
+++ configs/gateway/prepare-certbot.sh
@@ -1,33 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 
 # DOMAIN_NAME=$1 # "api2.gifts.hotdeals.dev"
 # EMAIL=$2 # "dealer@hotdeals.dev"
 
 
 if [ -z ${1+x} ]; then
-    echo "ERROR: The parameter in slot '1' for 'DOMAIN_NAME' is unset!";
+    echo "[ERROR]: The parameter in slot '1' for 'DOMAIN_NAME' is unset!";
     exit 1;
     else DOMAIN_NAME=$1;
 fi;
-echo "Domain name is: $DOMAIN_NAME"
+echo "[INFO]: Domain name is: $DOMAIN_NAME"
 
 if [ -z ${2+x} ]; then
-    echo "ERROR: The parameter in slot '2' for 'EMAIL' is unset!";
+    echo "[ERROR]: The parameter in slot '2' for 'EMAIL' is unset!";
     exit 1;
     else EMAIL=$2;
 fi;
-echo "Email is: $EMAIL"
-
-# Install certbot and its requirements
-# sudo apt-get update
-# sudo apt-get install certbot python3-certbot-nginx -y
+echo "[INFO]: Email is: $EMAIL"
 
 # Generate the tokens
-# sudo certbot --nginx -d $DOMAIN_NAME --non-interactive --agree-tos -m $EMAIL
+echo "Generating tokens..."
+certbot --nginx -d $DOMAIN_NAME --non-interactive --agree-tos --staging -m $EMAIL
 
 
 # Add cronjob for renewing certs - 30 days
-# crontab -e 0 12 * * * /usr/bin/certbot renew --quiet
+echo "[INFO]: Creating cron job..."
+echo "0 12 * * * /usr/bin/certbot renew --quiet" > mycron
+crontab mycron
 
-
-
+echo "[INFO]: Certbot script completed."
