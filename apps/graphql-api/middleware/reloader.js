f391b7400b3398cc4f65d5af04430ea98637554f
DryDish <57399961+DryDish@users.noreply.github.com>
2022-12-22 22:25:27 +0100

    add permission fix to middleware and update logging

--- apps/graphql-api/middleware/reloader.js
+++ apps/graphql-api/middleware/reloader.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { DatabaseConnection } from "../database/connection.js";
 import express from "express"
+import { fixDbPermissions } from "../database/fixPermissions.js";
 
 /**
  * Middleware to reload the DB connection if products.db has been modified.
@@ -17,20 +18,24 @@ const reloadOnFileChange = (req, res, next) => {
   console.log("[INFO]   Checking products.db's metadata...");
   fs.stat("/app/upload/products.db", async (err, stats) => {
     if (err) console.log(err);
-    console.log("[TRACE]  Local update time:  ", stats.mtime.toISOString());
-    console.log("[TRACE]  Stored update time: ", global.productsDbUpdatedAtTime);
+    console.log("[TRACE]  File mtime:                   ", stats.mtime.toISOString());
+    console.log("[TRACE]  productsDbUpdatedAtTime time: ", global.productsDbUpdatedAtTime);
 
     if (stats.mtime.toISOString() !== global.productsDbUpdatedAtTime) {
       console.log("[WARN]   The local file is out of date!");
+
+      console.log("[TRACE]  Fixing file permissions, just in case...")
+      fixDbPermissions();
+
       console.log("[WARN]   updating DB connection...");
       await DatabaseConnection.update();
-      console.log("[INFO]   DB connection updated");
 
+      console.log("[INFO]   Updating 'productsDbUpdatedAtTime'");
       global.productsDbUpdatedAtTime = stats.mtime.toISOString();
-      console.log("[INFO]   Stored update time updated");
 
-      console.log("[TRACE]  Stored update is now: ", global.productsDbUpdatedAtTime);
-      
+      console.log("[TRACE]  'productsDbUpdatedAtTime' is now: ", global.productsDbUpdatedAtTime);
+
+      console.log(("INFO]   Update complete, proceeding."));
       return next();
     }
     console.log("[INFO]   Local file is up to date, proceeding.");
