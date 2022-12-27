fc1bc7a37a6bd393c8e7391c0a126588d65791de
CristiPV <cristi.valentin29@gmail.com>
2022-12-27 16:59:56 +0100

    add database

--- apps/web-scraper/main.py
+++ apps/web-scraper/main.py
@@ -1,59 +1,53 @@
 import requests
 from pprint import pprint as print
+from database.database import Product, insert_product, generate_db, get_all_products
 from bs4 import BeautifulSoup
 
 
-class Product():
-    def __init__(self, price="", rating="", name=""):
-        self.price = price
-        self.rating = rating
-        self.name = name
+def scrape_main_page():
+    generate_db()
+    response = requests.get(
+        "https://www.pricerunner.dk/"
+    )
+    html = response.content
+    soup = BeautifulSoup(html, features="lxml")
 
-    def __repr__(self):
-        return f"| Price: {self.price}, Rating: {self.rating}, Name: {self.name} |"
+    content = soup.find(
+        "div",
+        {
+            "class": "R39C8j5r93"
+        }
+    )
 
-
-response = requests.get(
-    "https://www.pricerunner.dk/"
-)
-html = response.content
-soup = BeautifulSoup(html, features="lxml")
-products = []
-
-content = soup.find(
-    "div",
-    {
-        "class": "R39C8j5r93"
-    }
-)
-
-sections = content.find_all("div", {
-    "class": "CEV16XW373"
-})
-
-for section in sections:
-    product_list = section.find_all("div", {
-        "class": "al5wsmjlcK"
+    sections = content.find_all("div", {
+        "class": "CEV16XW373"
     })
 
-    for product_item in product_list:
-        product_price = product_item.find("span", {
-            "class": "pr-be5x0o"
+    for section in sections:
+        product_list = section.find_all("div", {
+            "class": "al5wsmjlcK"
         })
 
-        product_rating = product_item.find("p", {
-            "class": "pr-1ob9nd8"
-        })
+        for product_item in product_list:
+            product_price = product_item.find("span", {
+                "class": "pr-be5x0o"
+            })
 
-        product_name = product_item.find("h3", {
-            "class": "pr-7iigu3"
-        })
-        if product_price and product_name and product_rating:
-            product = Product(
-                price=product_price.text,
-                name=product_name.text,
-                rating=product_rating.text
-            )
-            products.append(product)
+            product_rating = product_item.find("p", {
+                "class": "pr-1ob9nd8"
+            })
 
-print(products)
+            product_name = product_item.find("h3", {
+                "class": "pr-7iigu3"
+            })
+            if product_price and product_name and product_rating:
+                product = Product(
+                    price=product_price.text,
+                    name=product_name.text,
+                    overall_rank=product_rating.text
+                )
+                insert_product(product)
+
+
+scrape_main_page()
+print(get_all_products())
