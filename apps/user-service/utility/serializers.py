f06378612dbb3962dd42e4818933049c7027280d
DryDish <57399961+DryDish@users.noreply.github.com>
2023-01-02 01:54:00 +0100

    fix parser uuid conversion

--- apps/user-service/utility/serializers.py
+++ apps/user-service/utility/serializers.py
@@ -6,6 +6,6 @@ class UUIDEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, uuid.UUID):
             # if the obj is uuid, we simply return the value of uuid
-            return o.hex
+            return str(o)
         # otherwise, encode as normal
         return json.JSONEncoder.default(self, o)
